1b6e5ea482bccd31f5bb6f329fb800edc87a936a
Berke Kanber <berkekanber@gmail.com>
2025-01-12 21:15:57 +0300

    feat: store example && zustand setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <Image src={Logo} alt="logo" width={200} height={200} />
+      <Image
+        src={Logo}
+        alt="logo"
+        width={200}
+        height={200}
+        priority
+        className="w-auto h-auto"
+      />
     </main>
   );
 }
