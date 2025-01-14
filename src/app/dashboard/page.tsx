d2a22fa29aaf86ee8fd820b5958422e10ee8e823
Berke Kanber <berkekanber@gmail.com>
2025-01-14 20:38:02 +0300

    feat: dashboard layout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,6 @@
 import { redirect } from "next/navigation";
 
-import { createClient } from "@/utils/fetch/server";
-import Playlists from "@/components/ui/playlists";
+import { createClient } from "@/utils/supabase/server";
 
 export default async function PrivatePage() {
   const supabase = await createClient();
@@ -12,8 +11,13 @@ export default async function PrivatePage() {
   }
 
   return (
-    <div>
-      <Playlists />
+    <div className="flex items-center justify-center h-full w-full text-white">
+      <div className="flex-shrink-0 flex-grow-0 basis-[15%] max-w-[15%] h-screen border-r border-gray-300/20">
+        1
+      </div>
+      <div className="flex-shrink-0 flex-grow-0 basis-[85%] max-w-[85%] h-screen">
+        2
+      </div>
     </div>
   );
 }
