a8e07c818b75e760f314f3f903304bf4e2ab9942
Berke Kanber <berkekanber@gmail.com>
2025-01-16 21:14:42 +0300

    refactor: menu items design

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -15,10 +15,10 @@ export default async function RootLayout({
   }
   return (
     <div className="flex items-center justify-center h-full w-full text-white">
-      <div className="flex-shrink-0 flex-grow-0 basis-[15%] max-w-[15%] h-screen border-r border-gray-300/20">
+      <div className="flex-shrink-0 flex-grow-0 basis-[12%] max-w-[12%] h-screen border-r border-gray-300/20">
         <Leftbar />
       </div>
-      <div className="flex-shrink-0 flex-grow-0 basis-[85%] max-w-[85%] h-screen py-6">
+      <div className="flex-shrink-0 flex-grow-0 basis-[88%] max-w-[88%] h-screen py-6">
         {children}
       </div>
     </div>
--- src/components/dashboard/menu-items.tsx
+++ src/components/dashboard/menu-items.tsx
@@ -2,54 +2,117 @@
 
 import { usePathname, useRouter } from "next/navigation";
 import React from "react";
+import { RiHome6Line } from "react-icons/ri";
+import { MdLibraryMusic } from "react-icons/md";
+import { BsMusicNoteList } from "react-icons/bs";
+import { TbTableSpark } from "react-icons/tb";
+import { FaChartBar, FaFire } from "react-icons/fa";
+import { LuCircleUser } from "react-icons/lu";
+import { IoIosSettings } from "react-icons/io";
+
+const menuItems = [
+  {
+    section: "DASHBOARD",
+    items: [
+      {
+        path: "/overview",
+        label: "Overview",
+        icon: <RiHome6Line className="text-xl" />,
+        isPremium: false,
+      },
+      {
+        path: "/playlists",
+        label: "Playlists",
+        icon: <MdLibraryMusic className="text-xl" />,
+        isPremium: false,
+      },
+      {
+        path: "/tracks",
+        label: "Tracks",
+        icon: <BsMusicNoteList className="text-xl" />,
+        isPremium: false,
+      },
+    ],
+  },
+  {
+    section: "STATISTICS",
+    items: [
+      {
+        path: "/chart",
+        label: "Chart",
+        icon: <FaChartBar className="text-xl" />,
+        isPremium: false,
+      },
+      {
+        path: "/tables",
+        label: "Tables",
+        icon: <TbTableSpark className="text-xl" />,
+        isPremium: false,
+      },
+      {
+        path: "/series",
+        label: "Series",
+        icon: <FaFire className="text-xl" />,
+        isPremium: true,
+      },
+    ],
+  },
+  {
+    section: "ACCOUNT",
+    items: [
+      {
+        path: "/profile",
+        label: "Profile",
+        icon: <LuCircleUser className="text-xl" />,
+        isPremium: false,
+      },
+      {
+        path: "/settings",
+        label: "Settings",
+        icon: <IoIosSettings className="text-xl" />,
+        isPremium: false,
+      },
+    ],
+  },
+];
 
 function MenuItems() {
   const router = useRouter();
   const pathName = usePathname();
 
-  function routerPages(path: any) {
+  const routerPages = (path: string) => {
     router.push(`/dashboard${path}`);
-  }
+  };
 
-  function isActive(path: any) {
-    return pathName === `/dashboard${path}`;
-  }
+  const isActive = (path: string) => pathName === `/dashboard${path}`;
 
   return (
     <div>
-      <ul>
-        <li
-          className={`hover:cursor-pointer ${
-            isActive("/overview") ? "text-green-500 font-bold" : "text-gray-700"
-          }`}
-          onClick={() => routerPages("/overview")}
-        >
-          Overview
-        </li>
-        <li
-          className={`hover:cursor-pointer ${
-            isActive("/page2") ? "text-green-500 font-bold" : "text-gray-700"
-          }`}
-          onClick={() => routerPages("/page2")}
-        >
-          home 2
-        </li>
-        <li
-          className={`hover:cursor-pointer ${
-            isActive("/page3") ? "text-green-500 font-bold" : "text-gray-700"
-          }`}
-          onClick={() => routerPages("/page3")}
-        >
-          home 3
-        </li>
-        <li
-          className={`hover:cursor-pointer ${
-            isActive("/page4") ? "text-green-500 font-bold" : "text-gray-700"
-          }`}
-          onClick={() => routerPages("/page4")}
-        >
-          home 4
-        </li>
+      <ul className="text-start">
+        {menuItems.map((section, index) => (
+          <div key={index} className="py-7">
+            <p className="font-bold text-gray-400 text-xs">{section.section}</p>
+            {section.items.map((item, itemIndex) => (
+              <li
+                key={itemIndex}
+                className={`hover:cursor-pointer font-medium text-md flex items-center gap-2 pt-3 ${
+                  isActive(item.path)
+                    ? "text-white"
+                    : "text-gray-400 hover:text-white"
+                } ${item.isPremium ? "hover:cursor-not-allowed" : ""}`}
+                onClick={() => routerPages(item.path)}
+              >
+                {item.icon}
+                {item.label}
+                {item.isPremium && (
+                  <span className="ml-2 px-2 py-1 text-xs text-white bg-green-600 rounded">
+                    Premium
+                  </span>
+                )}
+              </li>
+            ))}
+          </div>
+        ))}
       </ul>
     </div>
   );
