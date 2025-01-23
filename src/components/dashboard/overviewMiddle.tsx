17fe84076380650958e61ad943e486d1dcad0448
Berke Kanber <berkekanber@gmail.com>
2025-01-23 20:34:19 +0300

    feat: userTopArtists section

--- src/components/dashboard/overviewMiddle.tsx
+++ src/components/dashboard/overviewMiddle.tsx
@@ -1,7 +1,67 @@
-import React from "react";
+"use client";
+
+import { getUserTopArtists } from "@/stores/getUserTopArtists";
+import React, { useEffect } from "react";
+import toast from "react-hot-toast";
+import Skeleton from "@/components/dashboard/skeleton";
+import Image from "next/image";
 
 function OverviewMiddle() {
-  return <div>OverviewMiddle</div>;
+  const { data, loading, error, fetchData } = getUserTopArtists();
+
+  useEffect(() => {
+    const fetchTopItems = async () => {
+      await fetchData();
+    };
+
+    fetchTopItems();
+  }, [fetchData]);
+
+  if (loading) {
+    return <Skeleton className="w-full h-full" />;
+  }
+
+  if (error) {
+    toast.error(error);
+  }
+
+  console.log(data);
+
+  return (
+    <div>
+      {data && data?.items ? (
+        <h2 className="font-semibold text-lg ml-6">Your Top Artists</h2>
+      ) : null}
+      <div className="grid grid-cols-6 gap-6 p-4">
+        {data && data?.items
+          ? data.items
+              .sort((a: any, b: any) => a.popularity - b.popularity)
+              .map((item: any) => (
+                <div
+                  key={item.id}
+                  className="bg-neutral-800/20 flex flex-col rounded-xl overflow-hidden hover:scale-105 transition-all border-2 border-neutral-800"
+                >
+                  <div className="w-full h-40">
+                    <Image
+                      src={item?.images[0]?.url}
+                      alt="artists-image"
+                      width={200}
+                      height={200}
+                      className="w-full h-full object-cover"
+                    />
+                  </div>
+                  <p className="text-secondary font-medium text-xs text-center pt-4">
+                    Top #{item?.popularity}
+                  </p>
+                  <h2 className="font-semibold text-center text-sm p-4 text-white">
+                    {item?.name}
+                  </h2>
+                </div>
+              ))
+          : null}
+      </div>
+    </div>
+  );
 }
 
 export default OverviewMiddle;
--- src/types/storeState.ts
+++ src/types/storeState.ts
@@ -2,5 +2,5 @@ export interface storeState {
     data: any; 
     loading: boolean; 
     error: string | null; 
-    fetchData: (param?: string, param2?: string) => Promise<void>;
+    fetchData: (param?: string, param2?: string, param3?: number) => Promise<void>;
   }
